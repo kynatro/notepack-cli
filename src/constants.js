21ab65a9016e689134cc13a6a75e93eeb7bf4a22
Dave Shepard <kynatro@gmail.com>
2020-07-25 15:54:29 -0700

    Add src to FILE_IGNORE

--- src/constants.js
+++ src/constants.js
@@ -23,7 +23,7 @@ const BASE_FOLDERS = configuration.baseFolders;
  * @constant
  * @type {Array}
 */
-const FILE_IGNORE = ['.bin', '.git', '.vscode', 'node_modules', '.DS_Store', 'Thumbs.db', 'scripts', '.(ttf|woff)$', '.s?css$', '.sass$', '.js(on)?$', '.editorconfig', '.svg$', 'README.md']
+const FILE_IGNORE = ['.bin', '.git', '.vscode', 'node_modules', '.DS_Store', 'Thumbs.db', 'scripts', 'src', '.(ttf|woff)$', '.s?css$', '.sass$', '.js(on)?$', '.editorconfig', '.svg$', 'README.md']
 
 /**
  * Todo match pattern
