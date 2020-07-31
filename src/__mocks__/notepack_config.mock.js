d4f403828d9aff0941d2165b8fb86ebf63897bb0
Dave Shepard <kynatro@gmail.com>
2020-07-30 21:59:43 -0700

    Add notepack_config mock

---
+++ src/__mocks__/notepack_config.mock.js
@@ -0,0 +1,10 @@
+const path = require('path');
+
+module.exports = {
+  APP_ROOT_FOLDER: path.resolve('example'),
+  BASE_FOLDERS: ["Notes", "Team"],
+  TEAM_FOLDER: "Team",
+  TODO_ANCHOR_HEADING_LEVEL: '##',
+  TODO_ANCHOR: '## Open Todos',
+  TODO_GROUP_HEADING_LEVEL: '####'
+}
