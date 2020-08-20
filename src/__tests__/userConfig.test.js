f0a96324e7fc684d793023a81fc6eb9df430c67e
Dave Shepard <kynatro@gmail.com>
2020-08-20 12:45:01 -0700

    Stub userConfig.test

---
+++ src/__tests__/userConfig.test.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const notepackConfigMock = require('../__mocks__/notepack_config.mock')
+const userConfigModel = require('../userConfig');
+const { CONFIG_FILE_PATH, getUserConfig, readUserConfig, writeUserConfig } = userConfigModel;
+const fs = require('fs');
+const os = require('os');
+const path = require('path');
+
+jest.mock('fs');
+
+describe('getUserConfig()', () => {
+
+});
+
+describe('readUserConfig()', () => {
+
+});
+
+describe('writeUserConfig()', () => {
+
+});
