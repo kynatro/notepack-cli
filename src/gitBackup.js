a26d2798f35050d24cde64aec8c1725403dd69b2
dave.shepard <dave.shepard@servicenow.com>
2022-12-28 14:39:32 -0800

    Modify filtered to include root README.md

--- src/gitBackup.js
+++ src/gitBackup.js
@@ -122,7 +122,13 @@ async function stageFile(filePath) {
 (async () => {
   const statuses = await getStatuses();
   // Only process files in the user specified base folders
-  const filtered = statuses.filter((file) => BASE_FOLDERS.some(baseFolder => file.filePath.startsWith(baseFolder)));
+  const filtered = statuses.filter((file) => (
+    // File is in user's base folder(s)
+    BASE_FOLDERS.some(baseFolder => file.filePath.startsWith(baseFolder)) ||
+    // Root README.md file
+    file.filePath === 'README.md'
+  ));
+  // Include the root README.md file (which exists outside BASE_FOLDERS)
   let updates = [];
 
   if (!filtered.length) {
@@ -145,12 +151,6 @@ async function stageFile(filePath) {
 
   // Process updated files (checked off todos)
   if (updates.length) {
-    // Include the root README.md file (which exists outside BASE_FOLDERS)
-    updates.push({
-      status: STATUS_MODIFIED,
-      filePath: 'README.md'
-    });
-
     await asyncForEach(updates, async (file) => {
       const { filePath } = file;
       await stageFile(filePath);
