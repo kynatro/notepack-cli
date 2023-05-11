199adfdd328710c0a3679fd07f952204236f3b04
Dave Shepard <kynatro@gmail.com>
2023-05-10 22:37:21 -0700

    Configure main file

---
+++ index.js
@@ -0,0 +1,11 @@
+module.exports = {
+  constants: require('./src/constants'),
+  helpers: require('./src/helpers'),
+  recentFiles: require('./src/recentFiles'),
+  team: require('./src/team'),
+  todos: require('./src/todos'),
+  updateRecentFiles: require('./src/updateRecentFiles'),
+  updateTodos: require('./src/updateTodos'),
+  userConfig: require('./src/userConfig'),
+  default: require('./src/todos')
+};
